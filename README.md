0a5f18bca7780e036495c734cfdac4051f2e3abd
Your Name <you@example.com>
2023-03-14 15:40:36 -0700

    updated README. added screenshots. worked on syntax and code clean up. Will work on tests next

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Make-a-Logo
-Module 10: SVG Logo Maker
+Module 10: A command line application that uses node.js prompts to generate an SVG logo via the user inputs.
+
+## Installation
+1. Cloen the repository
+2. Install Node.js to your pc
+3. Within the command line/terminal, run npm install
+4. Follow the command line prompts after inputting node index.js 
+
+## Usage 
+* The purpose of this application is to create simple form logos for personal use. 
+* After the user follows the command line prompts using Node.js, within the 'lib' folder, a logo.svg file will be generated, showing an image within the browser. A sample image will be shown below.
+* For future application, this can be modified to generate more complex SVG style logos and designs by altering the code for the shapes. Rather than having simple shapes, the text/characters can be integrated within the logo itself. 
+* Example of a sample SVG logo that can be created:
+![Screenshot](./examples/sample-svg-logo.jpg)
+
+*Demonstration [Video of the application]()
+
+## Credits
+
+I would like to thank my ASKBCS learning assistant Marven for the great help when I was completely lost and [W3Schools](https://www.w3schools.com/?gclid=Cj0KCQjwtsCgBhDEARIsAE7RYh23GOF_0vlF06ht_LxkvbjxTPC0kmgQNPK6WKwqNRr6qHeXTrOtTVIaAsxREALw_wcB) for providing a very clear guide on how to use SVG shapes. 
+
+## Contact
+[Terri Mack](https://github.com/terrinmack?tab=repositories)
--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for text
-// THEN I can enter up to three characters
 
-// WHEN I am prompted for the text color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-
-// WHEN I am prompted for a shape
-// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
-
-// WHEN I am prompted for the shape's color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-
-// WHEN I have entered input for all the prompts
-// THEN an SVG file is created named `logo.svg`
-
-// AND the output text "Generated logo.svg" is printed in the command line
 // WHEN I open the `logo.svg` file in a browser
 // THEN I am shown a 300x200 pixel image that matches the criteria I entered
 
@@ -22,23 +6,32 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const {Circle, Square, Triangle} = require('./lib/shapes');
 
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for text
+// THEN I can enter up to three characters
 inquirer.prompt([
     {
         type: 'input',
         name: 'characters',
         message: 'Please enter up to 3 letters/characters.',
     },
+    // WHEN I am prompted for the text color
+    // THEN I can enter a color keyword (OR a hexadecimal number)
     {
         type: 'input',
         name: 'textColor',
         message: 'Please enter the color you would like your characters to be.',
     },
+    // WHEN I am prompted for a shape
+    // THEN I am presented with a list of shapes to choose from: circle, triangle, and square
     {
         type: 'list',
         name: 'shape',
         message: 'Please select a shape for you logo.',
         choices: ['circle', 'square', 'triangle',]
     },
+    // WHEN I am prompted for the shape's color
+    // THEN I can enter a color keyword (OR a hexadecimal number)
     {
         type: 'input',
         name: 'shapeColor',
@@ -48,9 +41,7 @@ inquirer.prompt([
 
 .then((data) => {
     const {characters, textColor, shape, shapeColor} = data;
-    
     var svgInstance;
-    
     switch (shape){
         case "circle":
             svgInstance = new Circle(shapeColor, textColor, characters);
@@ -60,6 +51,8 @@ inquirer.prompt([
             svgInstance = new Triangle(shapeColor, textColor, characters);   
     }
     
+    // WHEN I have entered input for all the prompts
+    // THEN an SVG file is created named `logo.svg`
     fs.writeFileSync('./examples/logo.svg', svgInstance.render());
     });
 
